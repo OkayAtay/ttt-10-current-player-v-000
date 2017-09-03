907dee9f0a39413e026a1614827b788e8292844d
Araba Taylor <araba.taylor20@gmail.com>
2017-09-03 00:21:16 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,10 +7,6 @@ def turn_count(board)
   end
   counter
 end
-def current_player(board)
-  if turn_count(board).even? == true
-    "X"
-  else
-    "O"
-  end
-end
+
+current_player(board) = turn_count(board).even? == true ? "X" : "O"
+
